be860430f01a59774b407b9331362a7ba9db568f
CAMILA ALVES <camila_alves3@estudante.sesisenai.org.br>
2024-09-30 19:27:12 -0300

    start project IOT with NetBeans

---
+++ ProjetoSaIoT/src/main/java/edu/senai/br/projetosaiot/ProjetoSaIoT.java
@@ -0,0 +1,12 @@
+package edu.senai.br.projetosaiot;
+
+/**
+ *
+ * @author camila_alves3
+ */
+public class ProjetoSaIoT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
