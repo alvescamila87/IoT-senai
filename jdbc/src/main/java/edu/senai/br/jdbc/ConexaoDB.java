2b130e8da2bc326cecc77890cade915b8686956b
CAMILA ALVES <alvescamila87@gmail.com>
2024-10-21 20:52:25 -0300

    create test connection

--- jdbc/src/main/java/edu/senai/br/jdbc/ConexaoDB.java
+++ jdbc/src/main/java/edu/senai/br/jdbc/ConexaoDB.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
  */
 public class ConexaoDB {
     
-    private static final String URL = "jdbc://mysql://localhost:3306/filmes_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/filmes_db";
     private static final String USER = "root";
     private static final String PASSWORD = "";
     
