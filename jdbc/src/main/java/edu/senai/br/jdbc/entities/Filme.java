3e61aace774ffce7efb5214b77fd998b31ebdf51
CAMILA ALVES <alvescamila87@gmail.com>
2024-10-21 19:40:37 -0300

    create entities

---
+++ jdbc/src/main/java/edu/senai/br/jdbc/entities/Filme.java
@@ -0,0 +1,62 @@
+
+package edu.senai.br.jdbc.entities;
+
+/**
+ *
+ * @author camila_alves3
+ */
+public class Filme {
+    
+    private int id;
+    private String titulo;
+    private int ano;
+    private String diretor;
+    private int categoria_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public int getCategoria_id() {
+        return categoria_id;
+    }
+
+    public void setCategoria_id(int categoria_id) {
+        this.categoria_id = categoria_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" + "id=" + id + ", titulo=" + titulo + ", ano=" + ano + ", diretor=" + diretor + ", categoria_id=" + categoria_id + '}';
+    } 
+    
+    
+}
