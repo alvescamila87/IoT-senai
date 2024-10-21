3a546bf3213f7e8fb7bacc19c3800b2ecc2084bb
CAMILA ALVES <alvescamila87@gmail.com>
2024-10-21 19:16:28 -0300

    create new project: jdbc

---
+++ jdbc/src/main/java/edu/senai/br/jdbc/Jdbc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.senai.br.jdbc;
+
+/**
+ *
+ * @author camila_alves3
+ */
+public class Jdbc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
