2f4aeec3dcbd71b8c32533428a426ab8cfc4d6ab
CAMILA ALVES <alvescamila87@gmail.com>
2024-11-04 19:16:34 -0300

    create cleanDataTableDao for movies

--- jdbc/src/main/java/edu/senai/br/jdbc/dao/CleanDataTableDAO.java
+++ jdbc/src/main/java/edu/senai/br/jdbc/dao/CleanDataTableDAO.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
  */
 public class CleanDataTableDAO {   
     
+    // Categoria
     public void deletarTabelaCategoria() throws SQLException {
         String sql = "DELETE FROM Categoria;";
         try (Connection conn = ConexaoDB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
@@ -18,10 +19,27 @@ public class CleanDataTableDAO {
         }
     }
     
+    // Categoria
     public void setAutoIncrementeOneTabelaCategoria() throws SQLException {
         String sql = "ALTER TABLE Categoria AUTO_INCREMENT=1;";
         try (Connection conn = ConexaoDB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.executeUpdate();
+        }
+    }
+    
+    // Filme
+    public void deletarTabelaFilme() throws SQLException {
+        String sql = "DELETE FROM Filme";
+        try (Connection connection = ConexaoDB.getConnection(); PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.executeUpdate();
+        }
+    }
+    
+    // Filme
+    public void setAutoIncrementOneTabelaFilme() throws SQLException {
+        String sql = "ALTER TABLE Filme AUTO_INCREMENT=1;";
+        try (Connection connection = ConexaoDB.getConnection(); PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.executeUpdate();
         }
     }
 }
